a7a2caece962457c7684a4625f33df989905e65e
Ionut Hada <hadaionut@gmail.com>
2020-11-24 21:27:33 +0200

    WIP getting the individual buildings to their own folders

--- scripts/lib/CityImporter.js
+++ scripts/lib/CityImporter.js
@@ -13,13 +13,25 @@ const prepareText = (rawText) => {
     return decodeHTML(rawText);
 }
 
-const createJournalEntry = async (entityName, rawText) => {
-    await JournalEntry.create({name: entityName, content: prepareText(rawText)})
+const createJournalEntry = async (entityName, rawText, folder) => {
+    await JournalEntry.create({name: entityName, content: prepareText(rawText), folder: folder})
 }
 
 const createCity = async (rawText) => {
     const jsonData = JSON.parse(rawText);
-    for (const attribute in jsonData) if (jsonData.hasOwnProperty(attribute)) await createJournalEntry(attribute, jsonData[attribute]);
+    for (const attribute in jsonData) {
+        if (!jsonData.hasOwnProperty(attribute)) continue;
+            if (typeof jsonData[attribute] !== 'string') {
+                //await Folder.create({name: 'a', type: 'journal'});
+                for (const secAttribute in jsonData[attribute]) {
+                    if (jsonData[attribute].hasOwnProperty(secAttribute))
+                        await createJournalEntry(secAttribute, jsonData[attribute][secAttribute], null);
+                }
+            }
+
+
+            await createJournalEntry(attribute, jsonData[attribute], null);
+    }
 }
 
 export {createCity}
