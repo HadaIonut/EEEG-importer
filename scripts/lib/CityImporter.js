3ed86399e5701cef3842370912e8626a4d957fdc
Ionut Hada <hadaionut@gmail.com>
2020-11-25 12:36:38 +0200

    WIP linking by id

--- scripts/lib/CityImporter.js
+++ scripts/lib/CityImporter.js
@@ -4,7 +4,16 @@ const decodeHTML = (rawText) => {
     return txt.value;
 }
 
-const prepareText = (rawText) => decodeHTML(rawText);
+const prepareText = (rawText) => {
+    const decoded = decodeHTML(rawText);
+    const $a = $('<div />', {html:decoded});
+    const located = $a.find('.link-internal');
+    located.replaceWith((index,text)=> {
+        const id = located[index]?.parentElement?.parentElement?.id || located[index]?.parentElement?.id;
+        return `@JournalEntry[${id}]{${text}}` || located[index];
+    })
+    return $a.html()
+}
 
 
 const createJournalEntry = async (entityName, rawText, folder) => {
@@ -12,17 +21,19 @@ const createJournalEntry = async (entityName, rawText, folder) => {
 }
 
 const createCity = async (rawText) => {
+    const uidToIdMap = new Map();
     const jsonData = JSON.parse(rawText);
     for (const attribute in jsonData) {
         if (!jsonData.hasOwnProperty(attribute)) continue;
-            if (typeof jsonData[attribute] !== 'string') {
-                const folder = await Folder.create({name: attribute, type: 'JournalEntry', parent: null});
-                for (const secAttribute in jsonData[attribute]) {
-                    if (jsonData[attribute].hasOwnProperty(secAttribute))
-                        await createJournalEntry(secAttribute, jsonData[attribute][secAttribute], folder.data._id);
-                }
+
+        if (typeof jsonData[attribute] !== 'string') {
+            const folder = await Folder.create({name: attribute, type: 'JournalEntry', parent: null});
+            for (const secAttribute in jsonData[attribute]) {
+                if (!jsonData[attribute].hasOwnProperty(secAttribute)) continue;
+                const newEntry = await createJournalEntry(jsonData[attribute][secAttribute].name, jsonData[attribute][secAttribute].output, folder.data._id);
+                uidToIdMap[secAttribute] = newEntry.data._id;
             }
-            else await createJournalEntry(attribute, jsonData[attribute], null);
+        } else await createJournalEntry(attribute, jsonData[attribute], null);
     }
 }
 
